5d9b703ff0978ef81851fa72fb0c867de85f6180
JudePark96 <judepark@kookmin.ac.kr>
2021-01-30 14:58:34 +0900

    add: Auditing Configuration

---
+++ src/main/java/com/nexters/winepick/base/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.nexters.winepick.base;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(value = AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
