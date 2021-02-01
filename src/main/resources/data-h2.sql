fb91dc1e428d5d3da24b94250a33fe4fe18c0d80
623nana <6230nana@gmail.com>
2021-02-01 11:03:12 +0900

    refactor: Entity명 변경

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,9 +1,9 @@
-insert into wine (nm_kor, nm_eng, country, price, ingredient, category, usage, degree, store, suit_temper, sweetness, acidity, body, tannin,suit_food)
+insert into wine (nm_kor, nm_eng, country, price, ingredient, category, purpose, degree, store, suit_temper, sweetness, acidity, body, tannin,suit_food)
 values ('쁘띠폴리', 'Petites Folies', '프랑스', 18000, '멜롯 60%, 까베네 소비뇽 40%', '레드와인', '테이블 와인', 13.5, '이마트 24', '16-18', 1, 3, 3, 3, '찹스테이크, 양고기');
-insert into wine (nm_kor, nm_eng, country, price, ingredient, category, usage, degree, store, suit_temper, sweetness, acidity, body, tannin,suit_food)
+insert into wine (nm_kor, nm_eng, country, price, ingredient, category, purpose, degree, store, suit_temper, sweetness, acidity, body, tannin,suit_food)
 values ('쁘띠폴리', 'Petites Folies', '프랑스', 18000, '멜롯 60%, 까베네 소비뇽 40%', '레드와인', '테이블 와인', 13.5, '이마트 24', '16-18', 1, 3, 3, 3, '찹스테이크, 양고기');
 
-insert into wine (nm_kor, nm_eng, country, price, ingredient, category, usage, degree, store, suit_temper, sweetness, acidity, body, tannin,suit_food)
+insert into wine (nm_kor, nm_eng, country, price, ingredient, category, purpose, degree, store, suit_temper, sweetness, acidity, body, tannin,suit_food)
 values ('쁘띠폴리', 'Petites Folies', '프랑스', 18000, '멜롯 60%, 까베네 소비뇽 40%', '레드와인', '테이블 와인', 13.5, '이마트 24', '16-18', 1, 3, 3, 3, '찹스테이크, 양고기');
 
 
