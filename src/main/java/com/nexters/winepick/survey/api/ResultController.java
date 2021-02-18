ee7f25bd1ec790db7beb19bbce53f55be067a829
623nana <6230nana@gmail.com>
2021-02-18 21:18:58 +0900

    refactor: header 속성 변경

--- src/main/java/com/nexters/winepick/survey/api/ResultController.java
+++ src/main/java/com/nexters/winepick/survey/api/ResultController.java
@@ -5,6 +5,7 @@ import com.nexters.winepick.survey.api.dto.ResultResponse;
 import com.nexters.winepick.survey.service.ResultService;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ public class ResultController {
 
   @GetMapping("/{resultId}")
   public BaseResponse<ResultResponse> getResult(HttpServletRequest req, @PathVariable("resultId") Integer resultId) {
-    String accessToken = req.getHeader("authorization");
+    String accessToken = req.getHeader(HttpHeaders.AUTHORIZATION);
     ResultResponse result = resultService.getResult(accessToken, resultId);
     return new BaseResponse<>(200, "0", result);
   }
